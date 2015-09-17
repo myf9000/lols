f90918713317f28cf2326f73ec0e63f8fb8e836f
myf9000 <myf9000@vp.pl>
2015-09-17 12:00:30 +0200

    First commit

---
+++ fitter/app/views/ads/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @ad, :id, :title, :description, :price, :adress, :created_at, :updated_at
---
+++ fitter/db/migrate/20150915190224_create_ads.rb
@@ -0,0 +1,12 @@
+class CreateAds < ActiveRecord::Migration
+  def change
+    create_table :ads do |t|
+      t.string :title
+      t.text :description
+      t.decimal :price
+      t.string :adress
+
+      t.timestamps null: false
+    end
+  end
+end
